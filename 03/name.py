04ca8850d0f7fd3602456bd8c2741bdecb9775df
dleh99 <83767383+dleh99@users.noreply.github.com>
2021-09-09 19:14:23 +0900

    3번 과제 입니다

---
+++ 03/name.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.circle(40)
+
+turtle.penup()
+turtle.goto(60, 100)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(120)
+
+turtle.penup()
+turtle.goto(120, 80)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(60)
+turtle.goto(120, 80)
+turtle.right(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+
+turtle.goto(150, 20)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(180)
+turtle.forward(120)
+
+turtle.exitonclick()
